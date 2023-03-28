cf86c15040fe89db88c591dbc5c2dca88a938cd5
hotsno <71658949+hotsno@users.noreply.github.com>
2023-03-28 09:19:48 -0700

    Update manga-resizer.sh

--- manga-resizer.sh
+++ manga-resizer.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
-unzip_d() {
-    zipfile="$1"
-    zipdir=${1%.cbz}
-    echo "Unzipping..."
-    unzip -d "$zipdir" "$zipfile" > /dev/null
-}
+### This script takes a .cbz file, and runs an ImageMagick command
+### to resize each image with a high quality filter (Lanczos) to the
+### resolution of your reading device. It will create an output .cbz
+### in your current directory.
+
+### Note: Need to have ImageMagick installed and in PATH. The `unzip`
+### and `zip` commands likely only work on macOS, but modifying for
+### other OS's should be straight forward.
+
+# Change this to your device's resolution
+resolution="1640x2360"
 
 if [[ ! -f $1 ]] || [[ $1 != *.cbz ]]; then
-    echo "Give a valid .cbz"
+    echo "Not a valid .cbz"
     exit 1
 fi
 
-# Unzip the original
-cbzDir="$(dirname $1)"
+cbzFile="$1"
 cbzBase="$(basename "$1" .cbz)"
 
-echo "Unzipping ${1}..."
+# Unzip the original
+echo "Unzipping '${cbzFile}'..."
 extractedDir="./${cbzBase}"
-unzip -d "$extractedDir" "$1" > /dev/null
+unzip -d "$extractedDir" "$cbzFile" > /dev/null
 
 # Resize all original images
-mkdir ./resized
+mkdir ./temp
 echo "Doing magick..."
 for f in "$extractedDir"/*; do
-    # echo "Resizing $file..."
-    magick "$f" -resize 1640x2360 "./resized/$(basename "$f")"
+    magick "$f" -resize "$resolution" "./temp/$(basename "$f")"
 done
 
 # Delete the unzipped original
 rm -rf "$extractedDir"
 
 # Create new .cbz
-echo "Creating new .cbz..."
-newCbz="./${cbzBase} RESIZED.cbz"
-zip -r -0 "$newCbz" ./resized > /dev/null
-echo -e "Created new .cbz at ${newCbz}!\n"
+newCbz="./${cbzBase} - Resized.cbz"
+zip -r -0 "$newCbz" ./temp > /dev/null
+echo -e "Created new .cbz at '${newCbz}'!\n"
 
 # Delete resized images
-rm -rf ./resized
+rm -rf ./temp
